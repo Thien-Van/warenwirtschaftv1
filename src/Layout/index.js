7733289b23460f215e35a9c984c270189fc8e09e
Thien-Van To <thienvan.to@gmail.com>
2021-10-20 15:38:03 +0200

    initialized project and deleted unnecessary files

---
+++ src/Layout/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Layout() {
+  return (
+    <div className="container">
+      <div className="container">
+        <h1>Warenwirtschaft</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
